88f12b407dcfa11fb193a6e8c855d855ec514540
Prajwal100 <raiparajwal500@gmail.com>
2020-09-02 16:41:49 +0545

    update

---
+++ lib/color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color primary = const Color(0xFF2A9DF4);
+Color secondary = const Color(0xFFDDE7EB);
+Color bg = const Color(0xFFFAFAFB);
--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,179 @@
 import 'package:flutter/material.dart';
 
+import 'dart:async';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:rollingdelivery/pages/HomePage.dart';
+import 'package:rollingdelivery/pages/OrderPage.dart';
+import 'package:rollingdelivery/pages/SettingPage.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:rollingdelivery/color.dart' as color;
+
+import 'Pages/HomePage.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MainPage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MainPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MainPageState createState() => _MainPageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _MainPageState extends State<MainPage> {
+  bool status1 = true;
+  int _currentIndex = 0;
+  List<Widget> _children = [];
+  List<Widget> _appBars = [];
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void initState() {
+    _children.add(OrderPage());
+    _children.add(OrderPage());
+    _children.add(OrderPage());
+    _children.add(SettingPage());
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    _appBars.add(_buildAppBar());
+    _appBars.add(_buildAppBarOne("Orders"));
+    _appBars.add(_buildAppBarOne("My History"));
+    _appBars.add(_buildAppBarSettings("Settings"));
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: _appBars[_currentIndex],
+      body: _children[_currentIndex],
+      bottomNavigationBar: _buildBottomNavigationBar(),
     );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      title: Text('Rolling Delivery',
+          style: GoogleFonts.poppins(
+              textStyle: TextStyle(fontWeight: FontWeight.w500))),
+      actions: [
+        IconButton(
+          icon: FaIcon(
+            FontAwesomeIcons.bell,
+            size: 20,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: FaIcon(
+            FontAwesomeIcons.solidUserCircle,
+            size: 20,
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAppBarSettings(String title) {
+    return AppBar(
+      bottom: PreferredSize(
+          child: Column(
+            children: [
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(70)),
+                    image: DecorationImage(
+                        image: ExactAssetImage('images/user.png'))),
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Container(
+                height: 30,
+                width: 85,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.all(Radius.circular(50))),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 6.0, vertical: 4),
+                  child: Center(
+                      child: Row(
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.solidCheckCircle,
+                        color: Colors.white,
+                        size: 14,
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text('Verified',
+                          style: GoogleFonts.poppins(
+                              textStyle: TextStyle(color: Colors.white))),
+                    ],
+                  )),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+          preferredSize: Size.fromHeight(130.0)),
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      title: Text(title,
+          style: GoogleFonts.poppins(
+              textStyle: TextStyle(fontWeight: FontWeight.w500))),
+    );
+  }
+
+  Widget _buildAppBarOne(String title) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      title: Text(title,
+          style: GoogleFonts.poppins(
+              textStyle: TextStyle(fontWeight: FontWeight.w500))),
+    );
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      onTap: _onTabTapped,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            title: Text("Dashboard",
+                style: GoogleFonts.robotoCondensed(fontSize: 16))),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            title: Text("Order",
+                style: GoogleFonts.robotoCondensed(fontSize: 16))),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            title: Text("History",
+                style: GoogleFonts.robotoCondensed(fontSize: 16))),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text("Settings",
+                style: GoogleFonts.robotoCondensed(fontSize: 16))),
+      ],
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _currentIndex,
+    );
+  }
+
+  _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
